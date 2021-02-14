b198853b84987f7bee42b1957f2caf12e392edbd
tsuyoshi1211 <31659600+tsuyoshi1211@users.noreply.github.com>
2021-02-14 18:41:32 +0900

    Delete inject.js

--- inject.js
+++
@@ -1,5 +0,0 @@
-const currentScript = document.currentScript || (function() {
-    const nodeList = document.getElementsByTagName('script')
-    return nodeList.item(nodeList.length - 1)
-}())
-alert(currentScript.getAttribute('data-slots'))
